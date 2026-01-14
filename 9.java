00e631b9a06d591c476ca00dda424a138f081540
Santoshabhi <159675295+Santoshabhi@users.noreply.github.com>
2026-01-14 20:24:48 +0530

    Given an integer x, return true if x is a palindrome, and false otherwise.

---
+++ 9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){return false;}
+        int r=0;
+        int s=x;
+        while(x>0){
+        r=(r*10)+(x%10);
+        x/=10;
+
+        }
+        return r==s;
+    }
+}
